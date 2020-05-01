8420584dd8cf9121370e926984cddcfd21d138c7
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-05-01 16:51:19 +0200

    display project filter category in home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,9 @@ class PagesController < ApplicationController
 
   def home
     @projects = Project.order(:created_at)
+    @tvs = Project.where(category: "TV")
+    @modes = Project.where(category: "Mode")
+    @events = Project.where(category: "Events")
+    @lives = Project.where(category: "Live")
   end
 end
