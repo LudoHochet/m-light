35acb45b5b0c0c42e020a9213af95f417fc9a289
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-05-01 12:16:20 +0200

    create and destroy method for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,35 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :destroy]
 
   def new
     @project = Project.new
   end
 
   def create
+    @project = Project.new(params_projects)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render new
+    end
+  end
 
+  def show
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def params_projects
+    params.require(:project).permit(:category, :name, :date, :location, :description, photos: [])
   end
 end
 
