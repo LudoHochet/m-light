ea5864aa7a2374958557bebccd748bcc9ac594a7
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-05-01 09:33:07 +0200

    config cloudinary

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ApplicationRecord
   CATEGORY = ["Mode", "TV", "Events", "Live"]
   validates :category, presence: true, inclusion: {in: CATEGORY}
   validates :name, presence: true
+  has_many_attached :photos
 end
