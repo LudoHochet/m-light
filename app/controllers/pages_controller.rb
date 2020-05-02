3874658aaa76866dd789d523c5fe3796016b2dce
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-05-02 23:43:50 +0200

    clean html and protect link authorized

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
     if params[:query].present?
       @projects = Project.search_global(params[:query])
     else
-      @projects = Project.order(:created_at)
+      # @projects = Project.order(:created_at)
       @tvs = Project.where(category: "TV")
       @modes = Project.where(category: "Mode")
       @events = Project.where(category: "Events")
