9966437da477936d0e0f6960e9291b08969426e5
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-05-03 15:12:39 +0200

    add route to contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/contact', to: 'pages#contact'
   resources :projects do
-
     member do
       delete :delete_photo_attachment
     end
-
     resources :illustrations, only: [:create] do
       member do
         delete :delete_photo_attachment
       end
     end
-
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
