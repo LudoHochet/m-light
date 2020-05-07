033838d00a8b8d7acb093469cb65225d2bf12c41
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-05-07 14:04:14 +0200

    mouseleave zone cards to light smooth the cards

--- app/javascript/components/light_cards.js
+++ app/javascript/components/light_cards.js
@@ -1,28 +1,15 @@
-// const lightCards = () => {
-//   const cardsZone = document.querySelector('.cards')
-//   const cards = document.querySelectorAll(".card-project");
-//   if(cardsZone) {
-//     cardsZone.addEventListener('mouseout', (event) => {
-//       cards.forEach((card) => {
-//           card.classList.remove('darker');
-//           card.classList.add('lighter');
-//           card.children[0].style.visibility = "hidden";
-//         });
-//     });
-//   }
-// };
+const lightCards = () => {
+  const cardsZone = document.querySelector('.zone')
+  const cards = document.querySelectorAll(".card-project");
+  if(cardsZone) {
+    cardsZone.addEventListener('mouseleave', (event) => {
+      cards.forEach((card) => {
+          card.classList.remove('darker');
+          card.classList.add('lighter-smooth');
+          card.children[0].style.visibility = "hidden";
+        });
+    });
+  }
+};
 
-
-// export { lightCards };
-
-
-
-// cards.forEach((card) => {
-//       card.addEventListener('mouseout', (event) => {
-//         cards.forEach((card) => {
-//           card.classList.remove('darker');
-//           card.classList.add('lighter');
-//           card.children[0].style.visibility = "hidden";
-//         });
-//       });
-//     });
+export { lightCards };
