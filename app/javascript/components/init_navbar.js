998b65eb7695f3dbea5dbff4cc0833c15b467b48
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-06-29 15:52:06 +0200

    add fadeIn for logo in homePage

--- app/javascript/components/init_navbar.js
+++ app/javascript/components/init_navbar.js
@@ -1,10 +1,15 @@
 const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('.logo');
+  const navbar = document.querySelector('.logoHome');
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
         console.log("hello");
-        navbar.style.visibility = 'visible';
+        navbar.classList.remove('invisible');
+        navbar.classList.add('fadeIn');
+      }
+      else {
+        navbar.classList.add('invisible');
+        navbar.classList.remove('fadeIn');
       }
     });
   }
