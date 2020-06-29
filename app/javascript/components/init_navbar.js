f715b287fe68c8d5dc0971a3b9492929f3a04a98
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-06-29 20:04:53 +0200

    try to fix the scroll result filter

--- app/javascript/components/init_navbar.js
+++ app/javascript/components/init_navbar.js
@@ -1,15 +1,25 @@
 const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('.logoHome');
-  if (navbar) {
+  const logo = document.querySelector('.logoHome');
+  const filters = document.querySelector('.filter-category');
+  const navbar = document.querySelector('.navbar-mlight');
+  if (logo) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
         console.log("hello");
-        navbar.classList.remove('invisible');
-        navbar.classList.add('fadeIn');
+        logo.classList.remove('invisible');
+        filters.classList.remove('invisible');
+        logo.classList.add('fadeIn');
+        filters.classList.add('fadeIn');
+        navbar.classList.remove('bg-transparent');
+        navbar.classList.add('bg-black');
       }
       else {
-        navbar.classList.add('invisible');
-        navbar.classList.remove('fadeIn');
+        logo.classList.add('invisible');
+        filters.classList.add('invisible');
+        logo.classList.remove('fadeIn');
+        filters.classList.remove('fadeIn');
+        navbar.classList.remove('bg-black');
+        navbar.classList.add('bg-transparent');
       }
     });
   }
