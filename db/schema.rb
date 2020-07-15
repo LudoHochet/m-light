cf09a81f64bbc6e63acd302d3fbb51807b8230b3
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-07-15 15:48:56 +0200

    remove last_name column in contact model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_03_130727) do
+ActiveRecord::Schema.define(version: 2020_07_15_134557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2020_05_03_130727) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
-    t.string "last_name"
     t.string "email"
     t.text "message"
     t.datetime "created_at", precision: 6, null: false
