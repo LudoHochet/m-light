0fe660b5762578cdff86c51e2e401a64e4f1431f
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-07-17 15:38:51 +0200

    still bug to make appaer carousel

--- app/javascript/components/size_flex.js
+++ app/javascript/components/size_flex.js
@@ -1,19 +1,31 @@
+const menu3D = document.querySelector('.menu-3D');
+
 const resizeFlex = () => {
   const items = document.querySelectorAll('.about-item');
     if (items) {
       items.forEach((item) => {
         item.addEventListener('click', (event) => {
           items.forEach((block) => {
+            // reduit tous les blocks et ajoute la class light
             block.classList.remove('open');
             block.classList.add('light');
+            // cache tous les contenus
             const contents = document.querySelectorAll('.content');
             contents.forEach((content) => {
               content.classList.add('d-none');
+              // if (menu3D) {
+                menu3D.classList.add('d-none');
+              // }
             })
           })
+          // agrandit la div sur laquelle on a cliqué
           event.currentTarget.classList.add('open');
           event.currentTarget.classList.remove('light');
-          event.currentTarget.children[event.currentTarget.children.length - 1].classList.remove('d-none');
+          // affiche son contenu. la div content est la 2ème de chaque div "about-item"
+          event.currentTarget.children[1].classList.remove('d-none');
+          if (event.currentTarget.children[2]) {
+            event.currentTarget.children[2].classList.remove('d-none');
+          }
         });
       })
     }
