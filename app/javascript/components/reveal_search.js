c368c0d2bf175b6b47db009efc434b618ef264c3
Ludovic Hochet <hochet.ludovic@gmail.com>
2020-11-26 16:46:45 +0100

    hide filters on serch clic

--- app/javascript/components/reveal_search.js
+++ app/javascript/components/reveal_search.js
@@ -1,9 +1,11 @@
 const revealSearch = () => {
   const searchIcon = document.querySelector('.search-icon');
+  const filters = document.querySelector('.filters')
   if (searchIcon) {
     const searchInput = document.getElementById('search-input');
     searchIcon.addEventListener('click', (event) => {
       searchInput.classList.toggle('d-none');
+      filters.classList.toggle('d-none');
     });
   }
 };
